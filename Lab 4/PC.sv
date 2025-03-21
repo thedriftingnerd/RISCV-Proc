642ba54414717d8b4097c993251ccd6f0cae9f11
rsmithiiee <137942077+rsmithiiee@users.noreply.github.com>
2025-03-21 12:24:58 -0700

    Update PC.sv

--- Lab 4/PC.sv
+++ Lab 4/PC.sv
@@ -3,14 +3,13 @@ module PC(
     input rst_n,
     input clk,
     input stall,
-    input [31:0] next_pc,
     output reg [31:0] pc
 );
     always @(posedge clk or negedge rst_n) begin
       if (!rst_n) begin
             pc <= 32'b0;
       end
-      else if (!stall) begin
+      else begin
             pc <= pc + 4;
       end
     end
